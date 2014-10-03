1c2ca681fa78240b2c66268173c04304e5b8e0bf
Han-Sheng Chin <hschin@gmail.com>
2014-10-04 00:07:11 +0800

    Create new offering report with offerings

--- app/controllers/administration/offering_reports_controller.rb
+++ app/controllers/administration/offering_reports_controller.rb
@@ -17,12 +17,22 @@ class Administration::OfferingReportsController < ApplicationController
 
   def create
     @offering_report = OfferingReport.new(offering_report_params)
-    @offering_report.save
+    @offering_report.monthly_cents = 0
+    @offering_report.missions_cents = 0
+    @offering_report.thanksgiving_cents = 0
+    @offering_report.fundraising_cents = 0
+    @offering_report.new_year_cents = 0
+    @offering_report.christmas_cents = 0
+    @offering_report.other_cents = 0
+    @offering_report.total_cents = 0
+    @offering_report.save!
   end
 
   private
 
   def offering_report_params
-    params.require(:offering_report).permit(:date, :monthly_cents, :missions_cents, :thanksgiving_cents, :fundraising_cents, :new_year_cents, :christmas_cents, :other_cents, :total_cents)
+    params.require(:offering_report).permit(:report_date, :monthly_cents, :missions_cents, :thanksgiving_cents, :fundraising_cents, :new_year_cents, :christmas_cents, :other_cents, :total_cents,
+      offerings_attributes: [:id, :date, :person_id, :family_id, :offering_type, :name, :amount_cents, :amount_currency, :offering_report_id]
+      )
   end
 end
--- app/models/offering_report.rb
+++ app/models/offering_report.rb
@@ -1,5 +1,7 @@
 class OfferingReport < ActiveRecord::Base
   has_many :offerings
 
+  accepts_nested_attributes_for :offerings, allow_destroy: true, reject_if: lambda { |a| a[:name].blank? }
+
   monetize :monthly_cents, :missions_cents, :thanksgiving_cents, :fundraising_cents, :new_year_cents, :christmas_cents, :other_cents, :total_cents
 end
